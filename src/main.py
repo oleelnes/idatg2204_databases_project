c6edded8023e91cdab9478bda7071ca7d463a5aa
andrbl <andrbl@stud.ntnu.no>
2022-03-21 18:59:04 +0100

    Added get_skis() func to retrive all skis and data stored in the product table.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST']="127.0.0.1"#"localhost"
 app.config['MYSQL_USER']="root"
 app.config['MYSQL_PASSWORD']=""
-app.config['MYSQL_DB']="banking"
+app.config['MYSQL_DB']="idatg2204_2022_group12"
 
 mysql = MySQL(app)
 
@@ -14,6 +15,17 @@ mysql = MySQL(app)
 def index():
     message = "Up and running!\nSee the endpoints:\n\n/public"
     return message
+
+@app.route('/public', methods=['GET'])
+def get_skis():
+    if request.method == 'GET':
+        cur = mysql.connection.cursor()
+        skis = cur.execute("SELECT * FROM `product`")
+        if skis > 0:
+            skis = cur.fetchall()
+        cur.close()
+        return jsonify(skis), 200
+    return "Sum ting went wong", 500
 
 if __name__ == '__main__':
     app.run(debug=True)
