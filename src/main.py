ef743efca73d3b18271fec2d7d3fd2a259bbc117
andrbl <andrbl@stud.ntnu.no>
2022-04-01 13:45:02 +0200

    Updated post_production_plan() func.

--- src/main.py
+++ src/main.py
@@ -82,13 +82,13 @@ def post_order_state():
                     legal = 1
             if legal == 1:
                 order = cur.execute("SELECT * FROM `order` WHERE id = " + orderid)
-                if order < 0:
+                if order > 0:
                     order.fetchall()
                     change_order_state = cur.execute("UPDATE `order` SET `order_status`="+ state)
                     mysql.connection.commit()
                 #cur.close()
                 order = cur.execute("SELECT * FROM `order` WHERE id = " + orderid)
-                if order < 0:
+                if order > 0:
                     order.fetchall() 
                 return jsonify(order), 201
             else:
@@ -112,25 +112,28 @@ def post_production_plan():
     type = content['type']
     productionAmount = content['productionAmount']
 
-    week = 4 * month
-    weeks = range(4)
-    for i in weeks:
-        plan = cur.execute("INSERT INTO `production_plan` (`week_number`, `manafacturer_id`) VALUES ('" + (week) + "', '200000')")
-        mysql.connection.commit()
-        typeData = cur.execute("INSERT INTO `production_type` \
-            (`production_week_number`, `product_id`, `day`, `type`, `production_amount`) \
-            VALUES ('%s', '%s', '%s', '%s', '%s')", week, productid, day, type, productionAmount)
-        mysql.connection.commit()
-        week += 1
-    prodplan = cur.execute("SELECT * FROM `production_type` WHERE production_week_number BETWEEN %s AND %s")
-    if prodplan < 0:
-        prodplan.fetchall() 
-        cur.close()
-        return jsonify(prodplan), 201
+    if month != "" and productid != "":
+        week = 4 * int(month)
+        weeks = range(4)
+        for i in weeks:
+            plan = cur.execute(f"INSERT INTO `production_plan` (`week_number`, `manafacturer_id`) VALUES ('{week}', '200000')")
+            mysql.connection.commit()
+            typeData = cur.execute(f"INSERT INTO `production_type` \
+                (`production_week_number`, `product_id`, `day`, `type`, `production_amount`) \
+                VALUES ('{week}', '{productid}', '{day}', '{type}', '{productionAmount}')")
+            mysql.connection.commit()
+            week += 1
+
+        prodplan = cur.execute(f"SELECT * FROM `production_type` WHERE production_week_number BETWEEN {week - 3} AND {week}")
+        if prodplan > 0:
+            prodplan = cur.fetchall()
+            cur.close()
+            return jsonify(prodplan), 201
     else:
         cur.close()
         return "Bad request", 404   
     return "Error in db", 500 #todo
+
 
 
 if __name__ == '__main__':
