f96aefcfff8fe460660edbc6a378bf6cb5754c9e
Ole Kristian Elnæs <ole.elnaes@gmail.com>
2022-05-13 23:45:52 +0200

    fixed manafacturer typo in database

--- src/endpoints/tests/test_customer.py
+++ src/endpoints/tests/test_customer.py
@@ -5,7 +5,7 @@ class test_customer(unittest.TestCase):
     CUSTOMER_ENDPOINT = "http://127.0.0.1:5000/customer"
     NEW_ORDER = CUSTOMER_ENDPOINT + "/orders/new"
 
-    def test_1_new_order(self):
+    def test_1_check_api(self):
         response = requests.gets(test_customer.NEW_ORDER)
         self.assertEqual(response.headers["Content-Type"],"application/json")
         self.assertEqual(response.status_code,200)
