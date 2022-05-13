c313ae588d1b12db39b434a7de670dd3326d9925
andrbl <andrbl@stud.ntnu.no>
2022-05-13 20:37:54 +0200

    Moved company endpoints into their own files in a seperate folder in the parent folder src. Moved the public endpoint functinality into its own file and folder in the parent dir src. Moved the transport functionality from main.py into its own file and folder in the parent dir src.

---
+++ src/public/public.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+
+
+def get_skis(mysql):
+    if request.method == 'GET':
+        cur = mysql.connection.cursor()
+        modelname = request.args.get('modelname')
+        if modelname:
+            skis = cur.execute("SELECT model, type, size, description, MSRPP ,url_photo \
+            FROM `product` \
+            WHERE model = %s", (modelname,))
+        else:        
+            skis = cur.execute("SELECT model, type, size, description, MSRPP ,url_photo FROM `product`")
+        if skis > 0:
+            skis = cur.fetchall()
+        cur.close()
+        return jsonify(skis), 200
+    return "Error in db", 500
