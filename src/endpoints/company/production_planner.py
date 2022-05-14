768c4ee4ba10adb53ba0388e8595ff57104878d0
andrbl <andrbl@stud.ntnu.no>
2022-05-14 15:15:40 +0200

    Updated post_production_plan() func in production_planner.py.

--- src/endpoints/company/production_planner.py
+++ src/endpoints/company/production_planner.py
@@ -3,37 +3,39 @@ from flask_mysqldb import MySQL
 
 # Adds a new production plan from post with json
 def post_production_plan(mysql):
-    cur = mysql.connection.cursor()
-    content = request.get_json()
-    if content:
-        month = content['month']
-        productid = content['productid']
-        day = content['day']
-        type = content['type']
-        productionAmount = content['productionAmount']
-        manufacturer_id = content['manufacturerid']
-    else:
-        cur.close()
-        return "Bad request", 404
-
-    if month != "" and productid != "":
-        week = 4 * int(month)
-        weeks = range(4)
-        for i in weeks:
-            plan = cur.execute("INSERT INTO `production_plan` (`week_number`, `manafacturer_id`) VALUES (%s, '%s')", (week, manufacturer_id))
-            mysql.connection.commit()
-            typeData = cur.execute("INSERT INTO `production_type` \
-                (`production_week_number`, `product_id`, `day`, `type`, `production_amount`) \
-                VALUES (%s, %s, %s, %s, %s)", (week, productid, day, type, productionAmount,))
-            mysql.connection.commit()
-            week += 1
-
-        prodplan = cur.execute("SELECT * FROM `production_type` WHERE production_week_number BETWEEN %s AND %s", (week - 3, week,))
-        if prodplan > 0:
-            prodplan = cur.fetchall()
+    if request.method == 'POST':
+        cur = mysql.connection.cursor()
+        content = request.get_json()
+        if content:
+            week = content['startweek']
+            productid = content['productid']
+            day = content['day']
+            type = content['type']
+            productionAmount = content['productionAmount']
+            manufacturer_id = content['manufacturerid']
+        else:
             cur.close()
-            return jsonify(prodplan), 201
-    else:
-        cur.close()
-        return "Bad request", 404   
-    return "Internal error in database", 500 #todo
+            return "Bad request", 404
+
+        if week != "" and int(week) < 50 and int(week) > 0 and productid != "" and day != "" and type != "" and productionAmount != "" and manufacturer_id != "":
+            weeks = range(4)
+            for i in weeks:
+                plan = cur.execute("INSERT INTO `production_plan` (`week_number`, `manafacturer_id`) VALUES (%s, %s)", (week, manufacturer_id,))
+                mysql.connection.commit()
+                typeData = cur.execute("INSERT INTO `production_type` \
+                    (`production_week_number`, `product_id`, `day`, `type`, `production_amount`) \
+                    VALUES (%s, %s, %s, %s, %s)", (week, productid, day, type, productionAmount,))
+                mysql.connection.commit()
+                week += 1
+
+            prodplan = cur.execute("SELECT * FROM `production_type` WHERE production_week_number BETWEEN %s AND %s", (week, week + 3,))
+            if prodplan > 0:
+                prodplan = cur.fetchall()
+                cur.close()
+                return jsonify(prodplan), 201
+        else:
+            cur.close()
+            return "Bad request", 404   
+    else: 
+        return "Wrong method. Only POST is supported.", 405
+    return "Internal error in database", 500 
