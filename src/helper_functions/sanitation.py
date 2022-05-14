db79e0abcadaab49ea97bec289bee4204bd4b71b
andrbl <andrbl@stud.ntnu.no>
2022-05-14 16:24:00 +0200

    Added type check in sanitation functions in sanitation.py.

--- src/helper_functions/sanitation.py
+++ src/helper_functions/sanitation.py
@@ -1,17 +1,29 @@
 import re
 
-def sanitize_input(str):
-    legal_input = "[^A-Za-z0-9]+"
-    return re.sub(legal_input, '', str)
+def sanitize_input(str_in):
+    if isinstance(str_in, str):
+        legal_input = "[^A-Za-z0-9]+"
+        return re.sub(legal_input, '', str_in)
+    else:
+        return str_in
         
-def sanitize_input_numbers(str):
-    legal_input = "[^0-9]+"
-    return re.sub(legal_input, '', str)
+def sanitize_input_numbers(str_in):
+    if isinstance(str_in, str):
+        legal_input = "[^0-9]+"
+        return re.sub(legal_input, '', str_in)
+    else:
+        return str_in
 
-def sanitize_input_date(str):
-    legal_input = "[^0-9-]+"
-    return re.sub(legal_input, '', str)
+def sanitize_input_date(str_in):
+    if isinstance(str_in, str):
+        legal_input = "[^0-9-]+"
+        return re.sub(legal_input, '', str_in)
+    else:
+        return str_in
 
-def sanitize_input_letters(str):
-    legal_input = "[^A-Za-z]+"
-    return re.sub(legal_input, '', str)
+def sanitize_input_letters(str_in):
+    if isinstance(str_in, str):
+        legal_input = "[^A-Za-z]+"
+        return re.sub(legal_input, '', str_in)
+    else:
+        return str_in
