d40c03705e847cab6bea19e51d6a2cd7e942a0f3
andrbl <andrbl@stud.ntnu.no>
2022-05-14 16:42:00 +0200

    Added option to search on size in public endpoint.

--- src/helper_functions/sanitation.py
+++ src/helper_functions/sanitation.py
@@ -2,7 +2,7 @@ import re
 
 def sanitize_input(str_in):
     if isinstance(str_in, str):
-        legal_input = "[^A-Za-z0-9]+"
+        legal_input = "[^A-Za-z0-9 ]+"
         return re.sub(legal_input, '', str_in)
     else:
         return str_in
@@ -23,7 +23,7 @@ def sanitize_input_date(str_in):
 
 def sanitize_input_letters(str_in):
     if isinstance(str_in, str):
-        legal_input = "[^A-Za-z]+"
+        legal_input = "[^A-Za-z ]+"
         return re.sub(legal_input, '', str_in)
     else:
         return str_in
