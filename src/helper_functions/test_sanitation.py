05d21fea9072525012c69299d651c694c27aac4b
andrbl <andrbl@stud.ntnu.no>
2022-05-14 17:49:39 +0200

    Completed tests for sanitation.py funcs.

--- src/helper_functions/test_sanitation.py
+++ src/helper_functions/test_sanitation.py
@@ -1,9 +1,15 @@
 import unittest
 import helper_functions.sanitation as help
 
-class TestSanitation(unittest.TestCase):
+class Test_Sanitation(unittest.TestCase):
     def test_sanitize_input(self):
-        self.assertEqual(help.sanitize_input("H2@|E.I"), "HEI", "Should be: HEI")
+        self.assertEqual(help.sanitize_input("H@|E.I"), "HEI", "Expected output: HEI")
+    
+    def test_sanitize_input_numbers(self):
+        self.assertEqual(help.sanitize_input_numbers("sad23]¥€"), "23", "Expected output: 23")
+    
+    def test_sanitize_input_date(self):
+        self.assertEqual(help.sanitize_input_date("2%022-02-02@+|"), "2022-02-02", "Expected output: 2022-02-02")
 
-#if __name__ == '__sanitation__':
-#    unittest.sanitation()
+    def test_sanitize_input_letters(self):
+        self.assertEqual(help.sanitize_input_letters("324asdE%||"), "asdE", "Expected output: asdE")
